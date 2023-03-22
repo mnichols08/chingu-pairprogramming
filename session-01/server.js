4f1a1bdd101e937a5122bf04ac34da1be03d4388
Mikey Nichols <mnix@journeytocode.io>
2023-03-22 15:57:48 -0400

    adds nodemon to devDependency list

--- session-01/server.js
+++ session-01/server.js
@@ -5,8 +5,8 @@ const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 const app = express();
 const port = 3000;
 
-const staticLorem = 'Lorem ipsum dolor sit amet';
-const lorem = new LoremIpsum({
+const loremPrefix = 'Lorem ipsum dolor sit amet';
+const loremIpsum = new LoremIpsum({
   sentencesPerParagraph: {
     max: 8,
     min: 4,
@@ -19,16 +19,15 @@ const lorem = new LoremIpsum({
 
 app.use(cors());
 
-app.get('/:length/:type/:static', (req, res) => {
-    const { length, type, static } = req.params;
+app.get('/:length/:type/:ipsum', (req, res) => {
+    const { length, type, ipsum } = req.params;
     const data = () => {        
-      console.log(length, type, static)
-        if (type === "words") return lorem.generateWords(Number(length));
-        if (type === "lists") return lorem.generateSentences(Number(length));
-        else return lorem.generateParagraphs(Number(length));
+        if (type === "words") return loremIpsum.generateWords(Number(length));
+        if (type === "lists") return loremIpsum.generateSentences(Number(length));
+        else return loremIpsum.generateParagraphs(Number(length));
     }
-    const paragraph = static == 'true' ? `${staticLorem} ${data()}` : data();
-    res.status(200).json({ paragraph, type, static });   
+    const paragraph = ipsum == 'true' ? `${loremPrefix} ${data()}` : data();
+    res.status(200).json({ paragraph, type, ipsum });   
 });
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
